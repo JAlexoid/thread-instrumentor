699f4191f6ae843ef9bf3deb5f0645610dec0949
Aleksandr Panzin <alex@panz.in>
2015-03-24 14:02:49 +0200

    Adding propagation tests

--- src/test/java/eu/activelogic/instrumentor/test/thread/vm/ThreadCallInterceptorIntegrationTest.java
+++ src/test/java/eu/activelogic/instrumentor/test/thread/vm/ThreadCallInterceptorIntegrationTest.java
@@ -1,15 +1,36 @@
+/*
+ *    Copyright 2015 Aleksandr Panzin (alex@activelogic.eu)
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package eu.activelogic.instrumentor.test.thread.vm;
 
 import static org.junit.Assert.*;
 
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 import eu.activelogic.instrumentor.thread.vm.ThreadCallInterceptor;
 import eu.activelogic.instrumentor.thread.vm.ThreadInterceptor;
 
+/**
+ * Some simple tests
+ * 
+ * @author Aleksandr Panzin
+ */
 public class ThreadCallInterceptorIntegrationTest {
+
+	private static final String THREAD_NAME = "TEST-THREAD";
 
 	static class MyInterceptor implements ThreadCallInterceptor {
 
@@ -113,6 +134,48 @@ public class ThreadCallInterceptorIntegrationTest {
 		assertTrue(r == m.createdRunnable);
 		assertTrue(t == m.startedThread);
 
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void threadStartExceptionShouldPropagete() {
+		ThreadInterceptor
+				.registerThreadInterceptor(new ThreadCallInterceptor() {
+
+					@Override
+					public void createThread(Thread t, Runnable r) {
+
+					}
+
+					@Override
+					public void callStart(Thread t) {
+						if (t.getName().equals(THREAD_NAME)) {
+							throw new RuntimeException();
+						}
+					}
+				});
+
+		Thread t = new Thread(THREAD_NAME);
+		t.start();
+	}
+
+	@Test(expected = RuntimeException.class)
+	public void threadCreateExceptionShouldPropagete() {
+		ThreadInterceptor
+				.registerThreadInterceptor(new ThreadCallInterceptor() {
+
+					@Override
+					public void createThread(Thread t, Runnable r) {
+						if (t.getName().equals(THREAD_NAME)) {
+							throw new RuntimeException();
+						}
+					}
+
+					@Override
+					public void callStart(Thread t) {
+					}
+				});
+
+		new Thread(THREAD_NAME);
 	}
 
 }
